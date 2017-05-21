b679df96d107e3bf1503f72ba43ebde086c8101c
balj55 <byronamh@gmail.com>
2017-05-21 16:03:36 -0600

    CRUD implementado

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,47 @@ var express = require('express')
 //------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------
 
+
+var admin = require("firebase-admin");
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./esalud-f8523-firebase-adminsdk-3yp2o-ec3ef26e7e.json");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://esalud-f8523.firebaseio.com/"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("/Data");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+
+
+var setData=function(arg,date,DiagnosticStr,id){
+	var usersRef = ref.child(id);
+	if(arg){
+		usersRef.set({
+  		ConsultaData: {
+    		Date: date,
+				cie10:"Lorem Ipsum",
+    		Diagnostico: DiagnosticStr
+  		}
+		});
+		return "Query con datos ingresados exitoso";
+	}else{
+		return "Error";
+	}
+}
+
+
+/** 
+ * NON ADMIN CODE -OLD-
+ * 
 var firebase = require("firebase");
 firebase.initializeApp({
   serviceAccount: "eSalud-534df159e7ce.json",
@@ -18,21 +59,7 @@ var ref = db.ref("restricted_access/secret_document");
 ref.once("value", function(snapshot) {
   console.log(snapshot.val());
 });
-
-var usersRef = ref.child("Data");
-var setData=function(arg,date,DiagnosticStr,id){
-	if(arg){
-		usersRef.set({
-  		id: {
-    		date: date,
-    		DiagnosticStr: DiagnosticStr
-  		}
-		});
-		return "Error al ingresar";
-	}else{
-		return "Query con datos ingresados exitoso";
-	}
-}
+*/
 
 
 //------------------------------------------------------------------------------------------------
