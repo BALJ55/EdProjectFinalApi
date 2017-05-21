14051afa920e50c0a37421defacba9784a3f6874
balj55 <byronamh@gmail.com>
2017-05-20 22:02:04 -0600

    integrado desde base tutorial

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,56 @@
 var express = require('express')
   , router = express.Router();
 
-  
-router.get('/', function(req, res) {
-	res.json({'users':'ALL'}); 
+
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+
+var firebase = require("firebase");
+firebase.initializeApp({
+  serviceAccount: "eSalud-534df159e7ce.json",
+  databaseURL: "https://esalud-f8523.firebaseio.com/"
 });
 
-router.get('/:id', function(req, res) {
-	res.json({'user_id':req.params.id}); 
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = firebase.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
 });
+
+var usersRef = ref.child("Data");
+var setData=function(arg,date,DiagnosticStr,id){
+	if(arg){
+		usersRef.set({
+  		id: {
+    		date: date,
+    		DiagnosticStr: DiagnosticStr
+  		}
+		});
+		return "Error al ingresar";
+	}else{
+		return "Query con datos ingresados exitoso";
+	}
+}
+
+
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+
+router.post('/', function(req,res){
+	if(!req.body.id){
+		return res.json({
+			msj:'error',
+			error:true
+		});
+	}
+	var result=setData(true,req.body.date,req.body.DiagnosticStr,req.body.id);
+	return res.json({
+		success: result
+	})
+
+});
+
 
 module.exports = router;
