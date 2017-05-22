a70c6284fc83b6f08126ebec77221c0e68874641
balj55 <byronamh@gmail.com>
2017-05-21 18:19:52 -0600

    integracion con api de oscar

--- controllers/users.js
+++ controllers/users.js
@@ -21,45 +21,54 @@ admin.initializeApp({
 var db = admin.database();
 var ref = db.ref("/Data");
 ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
+  //console.log(snapshot.val());
 });
 
 
 
+var request = require('request');
+
+
+//CREAR REGISTRO EN LA DB CON EL STRING INGRESADO
 var setData=function(arg,date,DiagnosticStr,id){
+	
 	var usersRef = ref.child(id);
+	global.Diagnostico=[];
+	var test=request.get('http://uvgproyectos.esaludgt.org/web/Api/Codigos?diagnostico='+DiagnosticStr,function (error, response, body) {
+  	      if (!error && response.statusCode == 200) {
+							var response=body;
+							usersRef.set({
+								ConsultaData: {
+								Date: date,
+								cie10:"Lorem Ipsum",
+								Diagnostico: JSON.parse(body)
+					
+							}
+						});
+        	}
+    	}
+	);
+	console.log(test.response);	
+	
+	//console.log("global: "+global.Diagnostico+" end");
+	
+	/*
 	if(arg){
 		usersRef.set({
   		ConsultaData: {
     		Date: date,
 				cie10:"Lorem Ipsum",
-    		Diagnostico: DiagnosticStr
+    		Diagnostico: DiagnosticoK,
+				
   		}
 		});
 		return "Query con datos ingresados exitoso";
 	}else{
 		return "Error";
 	}
+	*/
 }
 
-
-/** 
- * NON ADMIN CODE -OLD-
- * 
-var firebase = require("firebase");
-firebase.initializeApp({
-  serviceAccount: "eSalud-534df159e7ce.json",
-  databaseURL: "https://esalud-f8523.firebaseio.com/"
-});
-
-
-// As an admin, the app has access to read and write all data, regardless of Security Rules
-var db = firebase.database();
-var ref = db.ref("restricted_access/secret_document");
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
-});
-*/
 
 
 //------------------------------------------------------------------------------------------------
